8750da36fc7445e4e3a61516ebbe5b59c92337a1
fuliang <fuliang19890809@163.com>
2022-10-18 17:55:21 +0800

    提交

--- 日常工作/日常查询/日常查询.sql
+++ 日常工作/日常查询/日常查询.sql
@@ -11,14 +11,25 @@ ORDER BY u.account_id;
 # 查指定商品待发货清单
 SELECT
     op.product_name AS '商品名称',
-        op.count AS '数量',
+        sum(op.count) AS '数量',
         o.delivery_contact_name AS '联系人',
         o.delivery_contact_phone AS '联系电话',
-        CONCAT( o.delivery_province, o.delivery_city, o.delivery_district, o.delivery_town, o.delivery_address ) AS '收货地址'
+        CONCAT(
+            o.delivery_province,
+                o.delivery_city,
+                o.delivery_district,
+                o.delivery_town,
+                o.delivery_address
+            ) AS '收货地址'
 FROM
     order_products AS op
-        LEFT JOIN orders AS o ON o.`status` = 1
+        LEFT JOIN orders AS o ON o.id = op.order_id
 WHERE
-        op.original_product_id = 30
+        op.original_product_id = 28
+  AND o.`status` = 1
   AND op.deleted_at IS NULL
-  AND o.deleted_at IS NULL;
+  AND o.deleted_at IS NULL
+  AND o.delivery_contact_phone != ''
+group by
+    o.delivery_contact_phone;
+
